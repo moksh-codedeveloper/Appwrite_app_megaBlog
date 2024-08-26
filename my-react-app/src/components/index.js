1e03dcfac6eb0c7afc98380b79d15baa8576ff2e
MokshMalde <maldemoksh437@gmail.com>
2024-08-26 11:00:05 +0530

    new files added for components and for more elements

--- my-react-app/src/components/index.js
+++ my-react-app/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from './Container/Container.jsx';
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./LogoutBtn.jsx";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
 }
